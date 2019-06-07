1af25de6fd73c9fb699743b36f44c3002b988138
Matheus Tontini <tontini92@gmail.com>
2019-06-07 11:00:31 +0200

    controller

---
+++ resources/views/songs/add.php
@@ -0,0 +1,15 @@
+<form action="" method="post">
+
+    <label for="">Hash</label>
+    <input type="text" name="hash"><br>
+    <label for="">Author</label>
+    <input type="text" name="author"><br>
+    <label for="">Name</label>
+    <input type="text" name="name"><br>
+    <label for="">Description</label><br>
+    <textarea name="description" id="" cols="30" rows="10"></textarea>
+
+    <input type="submit" value="Submit">
+</form>
+
+<?= $something ?>
