be9f3a8102900fbd8ad225de9abd4a172de5af7e
Daniela Vazquez <daniela_056@hotmail.com>
2019-06-07 06:37:49 -0500

    list changes

--- resources/views/songs/list.php
+++ resources/views/songs/list.php
@@ -1,3 +1,12 @@
+<h1>List of Songs</h1>
+
 <?php foreach ($songs as $song) : ?>
-<?= $song->name ?>
+
+<p class="text-left"><?=$song->name?></p>
+<p class="text-left"><?='by ' .$song->author?></p>
+
+<a href="#" class="btn btn-success">play</a>
+<button type="button" class="btn btn-primary">edit</button>
+<button type="button" class="btn btn-danger">delete</button>
+
 <?php endforeach ; ?>
