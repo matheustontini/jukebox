56338c51317b998a7ac58591c20c642d7129b63d
Matheus Tontini <tontini92@gmail.com>
2019-06-07 13:10:43 +0200

    Bootstrap

--- resources/views/html_wrapper.php
+++ resources/views/html_wrapper.php
@@ -4,9 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Jukebox</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
+<nav class="nav">
+    <a class="nav-link" href="/songs/list">Songs</a>
+    <a class="nav-link" href="/songs/add">Add a song</a>
+    <a class="nav-link" href="#">Authors</a>
+    <a class="nav-link" href="#">Add an author</a>
+</nav>
+
     <?= $content ?>
 </body>
 </html>
