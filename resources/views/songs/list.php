0c9d294db25b61aa77e7d999ebdd5ca82e0cb041
Matheus Tontini <tontini92@gmail.com>
2019-06-07 15:05:24 +0200

    list

--- resources/views/songs/list.php
+++ resources/views/songs/list.php
@@ -1,10 +1,5 @@
-<h1>List of Songs</h1><?php foreach ($songs as $song) : ?><p class="text-left"><?=$song->name?></p>
+<h1>List of Songs</h1>
 
-<p class="text-left"><?='by ' .$song->author?></p><a href="/songs/play?id=<?=$song->id?>" class="btn btn-success">play</a>
-
-<<<<<<< HEAD
-<a href="/songs/add?id=<?=$song->id?>" class="btn btn-primary">edit</a>
-=======
 
 <?php foreach ($songs as $song) : ?>
 
@@ -14,6 +9,6 @@
 <a href="/songs/play?id=<?=$song->id?>" class="btn btn-success">play</a>
 <a href="/songs/add?id=<?=$song->id?>" class="btn btn-primary">edit</a>
 <a href="/songs/delete?id=<?=$song->id?>" class="btn btn-danger">delete</a>
->>>>>>> 860166718e2375aa8e7b38e6988e878e5c417bc1
 
-<a href="/songs/delete?id=<?=$song->id?>" class="btn btn-danger">delete</a><?php endforeach ; ?>
+
+<?php endforeach ; ?>
