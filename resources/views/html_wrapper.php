77f8c381273e9b90d239373b3ca0ce6b833e2683
Matheus Tontini <tontini92@gmail.com>
2019-06-07 15:21:56 +0200

    button

--- resources/views/html_wrapper.php
+++ resources/views/html_wrapper.php
@@ -7,19 +7,19 @@
 
     <title>Jukebox</title>
 
+
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
 <nav class="navbar bg-secondary py-4">
-
-    <a class="navbar-brand" href="#">JUKEBOX</a>
-    <a class="nav-link" href="/songs/list">Songs</a>
-    <a class="nav-link" href="/songs/add">Add a song</a>
-    <a class="nav-link" href="#">Authors</a>
-    <a class="nav-link" href="#">Add an author</a>
+    <a style="color: #fff" class="navbar-brand" href="#">JUKEBOX</a>
+    <a style="color: #fff" class="nav-link" href="/songs/list">Songs</a>
+    <a style="color: #fff" class="nav-link" href="/songs/add">Add a song</a>
+    <a style="color: #fff" class="nav-link" href="#">Authors</a>
+    <a style="color: #fff" class="nav-link" href="#">Add an author</a>
 </nav>
-
+<div style="margin:20px;">
     <?= $content ?>
-
+</div>
 </body>
 </html>
--- resources/views/songs/delete.php
+++ resources/views/songs/delete.php
@@ -1,5 +1,5 @@
 
 <form action="http://www.jukebox.test/songs/delete?id=<?=$song['id']?>" method="post">
     <?=csrf_field();?>
-    <input type="submit" value="DELETE">
+    <input type="submit" value="Delete" class="btn btn-danger">
 </form>
